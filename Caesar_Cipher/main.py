3c1c99ab4d80ea62109dc1ef13c5e65452a63729
RoninKing001 <roninking001@gmail.com>
2023-10-18 16:59:32 +1100

    Adding The Caesar Cipher

---
+++ Caesar_Cipher/main.py
@@ -0,0 +1,51 @@
+### ******************** The Caesar Cipher ********************
+### ********** About this Project
+"""
+This is a little bit more than a simple beginners project.
+It uses functions, loops and conditionals to work.
+
+There are better ways to create this project and I will update this project in the future.
+"""
+### ********** Code begins here
+
+# Importing
+from art import logo
+
+# Simple Greeting
+print("------------------------------------------------------------------------------------------")
+print("Welcome To The Caesar Cipher 🤖")
+print("------------------------------------------------------------------------------------------")
+
+# Alphabet
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+            'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+# Function
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+
+    if cipher_direction == "decode":
+        shift_amount *= -1
+
+    for char in start_text:
+
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+
+    print(f"Here's the {cipher_direction}d result: {end_text}")
+
+# While loop
+should_end = False
+
+while not should_end:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
